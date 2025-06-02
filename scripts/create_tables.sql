47e974950c48b8055e6f33aec962e9355a791fa7
Guilherme Faraco-laptop <guirfv29@gmail.com>
2025-06-02 14:19:00 -0300

    AGORA FOI

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS exercicios (
 CREATE TABLE IF NOT EXISTS planos (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(50) NOT NULL UNIQUE,
-    preco DECIMAL(10, 2) NOT NULL,
+    preco_mensal DECIMAL(10, 2) NOT NULL,
     duracao_meses INTEGER NOT NULL CHECK (duracao_meses > 0)
 );
 
@@ -17,6 +17,17 @@ CREATE TABLE IF NOT EXISTS instrutores (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(50) NOT NULL UNIQUE,
     especialidade VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS clientes (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL UNIQUE,
+    idade INTEGER NOT NULL CHECK (idade > 0),
+    sexo CHAR(1) CHECK (sexo IN ('M', 'F')),
+    email VARCHAR(100) NOT NULL UNIQUE,
+    telefone VARCHAR(15),
+    plano_id INTEGER NOT NULL,
+    FOREIGN KEY (plano_id) REFERENCES planos(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS treinos (
@@ -31,29 +42,20 @@ CREATE TABLE IF NOT EXISTS treinos (
     FOREIGN KEY (plano_id) REFERENCES planos(id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS clientes (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(50) NOT NULL UNIQUE,
-    idade INTEGER NOT NULL CHECK (duracao_meses > 0),
-    sexo CHAR(1) CHECK (sexo IN ('M', 'F')),
-    email VARCHAR(100) NOT NULL UNIQUE,
-    telefone VARCHAR(15),
-    plano_id INTEGER NOT NULL,
-    instrutor_id INTEGER NOT NULL,
-    treino_id INTEGER NOT NULL,
-    FOREIGN KEY (plano_id) REFERENCES planos(id) ON DELETE CASCADE,
-    FOREIGN KEY (instrutor_id) REFERENCES instrutores(id) ON DELETE CASCADE,
-    FOREIGN KEY (treino_id) REFERENCES treinos(id) ON DELETE CASCADE
-);
-
 CREATE TABLE IF NOT EXISTS treinos_exercicios (
-    id SERIAL PRIMARY KEY,
     treino_id INTEGER NOT NULL,
-    treino VARCHAR(50) NOT NULL,
     exercicio_id INTEGER NOT NULL,
-    exercicio VARCHAR(50) NOT NULL UNIQUE,
     series INTEGER NOT NULL CHECK (series > 0),
     repeticoes INTEGER NOT NULL CHECK (repeticoes > 0),
+    PRIMARY KEY (treino_id, exercicio_id),
     FOREIGN KEY (treino_id) REFERENCES treinos(id) ON DELETE CASCADE,
     FOREIGN KEY (exercicio_id) REFERENCES exercicios(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS pagamento_clientes (
+    id SERIAL PRIMARY KEY,
+    cliente_id INTEGER NOT NULL,
+    valor_pago DECIMAL(10,2) NOT NULL CHECK(valor_pago > 0),
+    data_pagamento DATE NOT NULL,
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id) ON DELETE CASCADE
 );
