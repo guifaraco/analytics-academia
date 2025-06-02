5dad1426e296b3c3fe57803bdeae56d73ea146b9
Guilherme Faraco-laptop <guirfv29@gmail.com>
2025-06-02 10:51:17 -0300

    Modificando gitignore

--- utils/database.py
+++ utils/database.py
@@ -5,9 +5,7 @@ from psycopg2 import sql
 import pandas as pd
 
 def get_connection():
-    """Estabelece conexão com o PostgreSQL"""
     try:
-        # Configuração para produção (Streamlit Cloud)
         if os.environ.get('POSTGRES_HOST'):
             conn = psycopg2.connect(
                 host=os.environ['POSTGRES_HOST'],
@@ -16,7 +14,6 @@ def get_connection():
                 user=os.environ['POSTGRES_USER'],
                 password=os.environ['POSTGRES_PASSWORD']
             )
-        # Configuração para desenvolvimento local
         else:
             conn = psycopg2.connect(
                 host=st.secrets.postgres.host,
@@ -31,7 +28,6 @@ def get_connection():
         return None
 
 def execute_query(query, params=None, return_df=False):
-    """Executa uma query no PostgreSQL"""
     conn = get_connection()
     if not conn:
         return None
@@ -42,7 +38,6 @@ def execute_query(query, params=None, return_df=False):
             
             if query.strip().lower().startswith(('select', 'with')):
                 if return_df:
-                    # Para queries SELECT retornar DataFrame
                     columns = [desc[0] for desc in cur.description]
                     data = cur.fetchall()
                     return pd.DataFrame(data, columns=columns)
